a9e0852ad7414e888410aa1b60508e2acef6b2ab
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-19 12:37:52 +0300

    feat: initialize passenger microservice

---
+++ src/main/java/org/modsen/passengermicroservice/PassengerMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package org.modsen.passengermicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassengerMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerMicroserviceApplication.class, args);
+    }
+}
