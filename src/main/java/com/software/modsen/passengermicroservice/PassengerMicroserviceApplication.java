ffec72ae0e626518c4a7e7c00ff8ed430a1e9feb
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-09-25 16:26:01 +0300

    chore: init eureka client

--- src/main/java/com/software/modsen/passengermicroservice/PassengerMicroserviceApplication.java
+++ src/main/java/com/software/modsen/passengermicroservice/PassengerMicroserviceApplication.java
@@ -2,10 +2,12 @@ package com.software.modsen.passengermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
+@EnableDiscoveryClient
 public class PassengerMicroserviceApplication {
     public static void main(String[] args) {
         SpringApplication.run(PassengerMicroserviceApplication.class, args);
