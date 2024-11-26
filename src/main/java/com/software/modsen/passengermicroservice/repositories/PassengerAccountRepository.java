b3f153ad32d7913458bdddfc0438fa65ac7694f4
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-11-26 10:58:04 +0300

    chore: update repositories from mongo repositories to reactive mongo repositories

--- src/main/java/com/software/modsen/passengermicroservice/repositories/PassengerAccountRepository.java
+++ src/main/java/com/software/modsen/passengermicroservice/repositories/PassengerAccountRepository.java
@@ -1,12 +1,15 @@
 package com.software.modsen.passengermicroservice.repositories;
 
 import com.software.modsen.passengermicroservice.entities.account.PassengerAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.software.modsen.passengermicroservice.entities.rating.PassengerRating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 @Repository
-public interface PassengerAccountRepository extends JpaRepository<PassengerAccount, Long> {
-    Optional<PassengerAccount> findByPassengerId(long passengerId);
+public interface PassengerAccountRepository extends ReactiveMongoRepository<PassengerAccount, String> {
+    Mono<PassengerAccount> findByPassengerId(String passengerId);
 }
--- src/main/java/com/software/modsen/passengermicroservice/repositories/PassengerRepository.java
+++ src/main/java/com/software/modsen/passengermicroservice/repositories/PassengerRepository.java
@@ -1,16 +1,18 @@
 package com.software.modsen.passengermicroservice.repositories;
 
 import com.software.modsen.passengermicroservice.entities.Passenger;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 import java.util.Optional;
 
 @Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-    Optional<Passenger> findPassengerByIdAndIsDeleted(long id, boolean isDeleted);
+public interface PassengerRepository extends ReactiveMongoRepository<Passenger, String> {
+    Mono<Passenger> findPassengerByIdAndDeleted(String id, boolean isDeleted);
 
-    boolean existsByIdAndIsDeleted(long id, boolean isDeleted);
+    Mono<Boolean> existsByIdAndDeleted(String id, boolean isDeleted);
 
-    Optional<Passenger> findByName(String name);
+    Mono<Passenger> findByName(String name);
 }
