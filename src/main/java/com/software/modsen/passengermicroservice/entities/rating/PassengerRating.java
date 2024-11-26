0a1055d0b658d05e93f9ad50484838bc20c46661
AlexeyKriva <alexey.kriva03.com@gmail.com>
2024-11-26 10:56:46 +0300

    feat: update entities

--- src/main/java/com/software/modsen/passengermicroservice/entities/rating/PassengerRating.java
+++ src/main/java/com/software/modsen/passengermicroservice/entities/rating/PassengerRating.java
@@ -2,32 +2,28 @@ package com.software.modsen.passengermicroservice.entities.rating;
 
 import com.software.modsen.passengermicroservice.entities.Passenger;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
 
-@Entity
-@Table(name = "passenger_rating")
+@Document(collection = "ratings")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 @Schema(description = "Passenger rating entity.")
 public class PassengerRating {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
-    private long id;
+    private String id;
 
-    @OneToOne
-    @JoinColumn(name = "passenger_id")
-    private Passenger passenger;
+    private String passengerId;
 
-    @Column(name = "rating_value", nullable = false)
     private Float ratingValue;
 
-    @Column(name = "number_of_ratings", nullable = false)
     private Integer numberOfRatings;
 }
